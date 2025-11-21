7b4daa5e98567df8f028cd1588b10f4d4da22591
Japmjr <chuchitoperdomo@gmail.com>
2025-11-21 15:46:09 +0000

    arreglos

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -13,6 +13,9 @@ import Home from './components/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Home/> 
+
+
+    
   </React.StrictMode>,
 )
